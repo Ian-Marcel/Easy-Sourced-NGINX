36409d0584c9c519422934498a12f8018f3e205b
Ian <ianmarcel@mailfence.com>
2024-09-14 07:37:12 -0300

    07:37:12 - 14/09/2024

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo rm -f /usr/sbin/nginx
+sudo rm -rf /usr/lib/nginx/modules
+sudo rm -rf /etc/nginx
+sudo rm -rf /var/log/nginx
+sudo rm -rf /var/cache/nginx
+sudo rm -f /var/run/nginx.pid
+sudo rm -f /var/run/nginx.lock
+sudo rm -f /etc/systemd/system/nginx.service
+sudo systemctl daemon-reload
