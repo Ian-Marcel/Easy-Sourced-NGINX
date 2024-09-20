d35e7429b98e2d9caf24374ab3ae2b3088e279bc
Ian <ianmarcel@mailfence.com>
2024-09-19 23:46:11 -0300

    23:46:11 - 19/09/2024

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ Continuando...
 fi
 
 sudo mkdir -pv /usr/lib/nginx/modules /etc/nginx /var/log/nginx /var/cache/nginx &&
-sudo chown -R nginx:nginx /usr/lib/nginx/modules /etc/nginx /var/log/nginx /var/cache/nginx
+    sudo chown -R nginx:nginx /usr/lib/nginx/modules /etc/nginx /var/log/nginx /var/cache/nginx
 
 echo "Dependências satisfeitas"
 
