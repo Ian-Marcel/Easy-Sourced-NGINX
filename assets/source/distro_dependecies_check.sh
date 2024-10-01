3d2701dc30e1402b36adfaa1cdd7e4431aa19e50
Ian <ianmarcel@mailfence.com>
2024-10-01 14:23:41 -0300

    14:23:41 - 01/10/2024

--- assets/source/distro_dependecies_check.sh
+++ assets/source/distro_dependecies_check.sh
@@ -16,8 +16,8 @@ if [ "$DISTRO" = "debian" ] || [ "$DISTRO" = "ubuntu" ]; then
     sudo apt update -y && sudo apt upgrade -y &&
         sudo apt -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl3 libssl-dev libxml2 libxslt1-dev
 
-elif [ "$DISTRO" = "fedora" ] || [ "$DISTRO" = "rocky" ] || [ "$DISTRO" = "alma" ]; then
-    echo "  RHEL-family ( Fedora, RHEL, Alma ... )"
+elif [ "$DISTRO" = "fedora" ] || [ "$DISTRO" = "\"rocky\"" ] || [ "$DISTRO" = "\"alma\"" ]; then
+    echo "  RHEL-family ( Fedora, RHEL, CentOS ... )"
     sudo dnf update -y &&
         sudo dnf -y group install "Development Tools" &&
         sudo dnf -y install pcre pcre-devel zlib zlib-devel openssl openssl-devel libxml2 libxslt-devel
