863ea08685903ae293d179f93ed2ba572b18efc6
Ian <ianmarcel@mailfence.com>
2024-10-01 14:14:15 -0300

    14:14:15 - 01/10/2024

--- assets/source/distro_dependecies_check.sh
+++ assets/source/distro_dependecies_check.sh
@@ -8,15 +8,15 @@ if ! command -v lsb_release &>/dev/null; then
     exit 1
 fi
 
-DISTRO=$(lsb_release -i | awk '{print $3}')
+DISTRO=$(grep -w ID /etc/os-release | awk -F= '{print $2}')
 
 echo "System:"
-if [ "$DISTRO" = "Debian" ] || [ "$DISTRO" = "Ubuntu" ]; then
+if [ "$DISTRO" = "debian" ] || [ "$DISTRO" = "ubuntu" ]; then
     echo "  Debian family ( Debian, Ubuntu, Proxmox ... )"
     sudo apt update -y && sudo apt upgrade -y &&
         sudo apt -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl3 libssl-dev libxml2 libxslt1-dev
 
-elif [ "$DISTRO" = "Fedora" ]; then
+elif [ "$DISTRO" = "fedora" ] || [ "$DISTRO" = "rocky" ] || [ "$DISTRO" = "alma" ]; then
     echo "  RHEL-family ( Fedora, RHEL, Alma ... )"
     sudo dnf update -y &&
         sudo dnf -y group install "Development Tools" &&
