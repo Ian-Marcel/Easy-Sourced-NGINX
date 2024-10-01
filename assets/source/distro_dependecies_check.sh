1bb2ee9fbe99195f3a11fc40f82a2e848203c824
Ian <ianmarcel@mailfence.com>
2024-10-01 14:24:57 -0300

    14:24:57 - 01/10/2024

--- assets/source/distro_dependecies_check.sh
+++ assets/source/distro_dependecies_check.sh
@@ -12,7 +12,7 @@ DISTRO=$(grep -w ID /etc/os-release | awk -F= '{print $2}')
 
 echo "System:"
 if [ "$DISTRO" = "debian" ] || [ "$DISTRO" = "ubuntu" ]; then
-    echo "  Debian family ( Debian, Ubuntu, Proxmox ... )"
+    echo "  Debian family ( Debian, Ubuntu ... )"
     sudo apt update -y && sudo apt upgrade -y &&
         sudo apt -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl3 libssl-dev libxml2 libxslt1-dev
 
