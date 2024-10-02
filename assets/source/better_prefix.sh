71285204bf6e326210669f4605285c26460bd665
Ian <ianmarcel@mailfence.com>
2024-10-02 13:58:27 -0300

    13:58:27 - 02/10/2024

--- assets/source/better_prefix.sh
+++ assets/source/better_prefix.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 echo ''
-shopt -s nocasematch &&
-    while true; do
-        read -rp "We offer an optimized nginx configuration, do you want it applied? [(Y)es/(n)o]: " NGINX_BETTER_PREFIX &&
-            if [ "$NGINX_BETTER_PREFIX" = S ] || [ "$NGINX_BETTER_PREFIX" = Sim ]; then
-                echo "Ok, applying new configuration..."
-                sudo rm -rf /etc/nginx &&
-                    tar -zxf "$ESNx_ASSETS/file/nginx.tar.gz" &&
-                    sudo cp -r nginx /etc/
-                sudo mkdir -p /var/www/nginx &&
-                    sudo cp "$ESNx_ASSETS/file/index.html" /var/www/nginx/ &&
-                    sudo chown -R nginx:nginx /var/www/nginx &&
-                    echo "New configuration applied!"
-                break
-            elif [ "$NGINX_BETTER_PREFIX" = N ] || [ "$NGINX_BETTER_PREFIX" = Não ]; then
-                echo "Ok, continuing with default settings!"
-                break
-            else
-                echo "Please answer with (Y)es or (N)o."
-            fi
-    done &&
+shopt -s nocasematch
+while true; do
+    read -rp "We offer an optimized nginx configuration, do you want it applied? [(Y)es/(n)o]: " NGINX_BETTER_PREFIX &&
+        if [ "$NGINX_BETTER_PREFIX" = Y ] || [ "$NGINX_BETTER_PREFIX" = Yes ]; then
+            echo "Ok, applying new configuration..."
+            sudo rm -rf /etc/nginx &&
+                tar -zxf "$ESNx_ASSETS/file/nginx.tar.gz" &&
+                sudo cp -r nginx /etc/
+            sudo mkdir -p /var/www/nginx &&
+                sudo cp "$ESNx_ASSETS/file/index.html" /var/www/nginx/ &&
+                sudo chown -R nginx:nginx /var/www/nginx &&
+                echo "New configuration applied!"
+            break
+        elif [ "$NGINX_BETTER_PREFIX" = N ] || [ "$NGINX_BETTER_PREFIX" = No ]; then
+            echo "Ok, continuing with default settings!"
+            break
+        else
+            echo "Please answer with (Y)es or (N)o."
+        fi
+done
 shopt -u nocasematch
