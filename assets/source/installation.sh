23573fdb5c66c39d9c0c47867719113cbb22a181
Ian <ianmarcel@mailfence.com>
2024-11-23 01:39:55 -0300

    01:39:55 - 23/11/2024

--- assets/source/installation.sh
+++ assets/source/installation.sh
@@ -2,7 +2,7 @@
 
 ## Construindo, compilando e instalando a configuração NGINX #######################
 echo -e "\n${BGREEN}Package and modules obtained! ${BCYAN}Configuring NGINX... ${NC} \n"
-cd nginx-* || exit
+cd nginx-1.26.2 || exit
 source configure.sh &&
     echo -e "${BGREEN}NGINX configured! ${BCYAN}Compiling NGINX... ${NC} \n" &&
     make &&
