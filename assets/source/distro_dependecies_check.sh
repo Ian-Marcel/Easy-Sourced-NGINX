2043d2a3b5c3873fd1d9f8c14b5afcc4f0cefefc
Ian <ianmarcel@mailfence.com>
2024-12-30 15:11:46 -0300

    15:11:46 - 30/12/2024

--- assets/source/distro_dependecies_check.sh
+++ assets/source/distro_dependecies_check.sh
@@ -7,8 +7,8 @@ DISTRO_ID=$(grep -w ID /etc/os-release | awk -F= '{gsub(/"/, "", $2); print $2}'
 echo -e "\n${BCYAN}System: ${NC}"
 if [ "$DISTRO_ID" = "debian" ] || [ "$DISTRO_ID" = "ubuntu" ]; then
     echo -e "  ${BGREEN}Debian family ( Debian, Ubuntu, Raspberry Pi OS ... ) ${NC} \n"
-    sudo apt update -y && sudo apt upgrade -y &&
-        sudo apt -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl3 libssl-dev libxml2 libxslt1-dev
+    sudo apt-get update -y && sudo apt-get upgrade -y &&
+        sudo apt-get -y install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl3 libssl-dev libxml2 libxslt1-dev
 
 elif [ "$DISTRO_ID" = "fedora" ] || [ "$DISTRO_ID" = "rocky" ] || [ "$DISTRO_ID" = "almalinux" ]; then
     echo -e "  ${BGREEN}Red Hat family ( Fedora, RHEL, CentOS ... ) ${NC} \n"
