13a2df46ab1060f4f4195b5a5861a4da6f49e03e
Ian <ianmarcel@mailfence.com>
2025-01-01 14:55:22 -0300

    14:55:22 - 01/01/2025

--- assets/source/better_prefix.sh
+++ assets/source/better_prefix.sh
@@ -2,7 +2,7 @@
 
 shopt -s nocasematch
 while true; do
-    if [[ -v "$CURL_ESX" && "$CURL_ESX" = 2 ]]; then
+    if [[ -v "$CURL_ESX" ]] && [[ "$CURL_ESX" = 2 ]]; then
         NGINX_BETTER_PREFIX='No'
         break
     else
