86f3a5299139a00a0c27c77b68d15bae40c05248
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-05-18 16:34:05 -0300

    correção downloading_packages.sh:4

--- assets/source/downloading_packages.sh
+++ assets/source/downloading_packages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo -e "${BGREEN}Dependencies satisfied. ${BCYAN}Getting NGINX package and extra unofficial modules... ${NC} \n"
-wget --directory-prefix "$ESNx_TMP" --quiet --input-file "$ESNx_ASSETS/file/packages.ini" &&
-    for tarballs in *.tar.gz; do
+wget --directory-prefix "$ESNx_TMP" --quiet --input-file "$ESNx_ASSETS/file/packages" &&
+    for tarballs in *.tar.gz ; do
         tar -zxf "$tarballs" &&
             rm "$tarballs"
     done
