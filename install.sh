65da4fe6974145267178db08b53ee86882b0875e
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-05-19 16:43:17 -0300

    16:43:17 - 19/05/2025

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ BGREEN='\033[1;32m'
 BYELLOW='\033[1;33m'
 BRED='\033[1;31m'
 
+trap 'echo -e "❌ ${BYELLOW}Error occurred.${BCYAN} Exiting.${NC}"' ERR
+
 ## Designando variáveis de ambiente #######################
 mkdir -p tmp assets &&
     ESNx=$(pwd) &&
