80f0da1df77e1baba7b130017b440d0be87d1d56
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-05-30 15:15:04 -0300

    reinstall.sh created, pipefail added to uninstall.sh

---
+++ reinstall.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail # Sair em caso de erro e falha em variáveis ​​não definidas
+
+NC='\033[0m' # No Color
+BCYAN='\033[1;36m'
+BGREEN='\033[1;32m'
+BYELLOW='\033[1;33m'
+BRED='\033[1;31m'
+
+trap 'echo -e "❌ ${BYELLOW}Error occurred! ${BCYAN}Exiting...${NC}"' ERR
+
+echo -e "${BCYAN}Backing up current nginx configuration folder, ${BYELLOW}it requires root access! ${NC}"
+sudo tar -zcf nginx-backup.tar.gz -C /etc/ nginx
+sudo chown $USER:$USER nginx-backup.tar.gz
+source uninstall.sh
+source install.sh
+sudo rm -r /etc/nginx
+tar -zxf nginx-backup.tar.gz
+sudo mv nginx /etc/
+rm nginx-backup.tar.gz
--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-sudo systemctl disable --now nginx.service &&
+set -euo pipefail # Sair em caso de erro e falha em variáveis ​​não definidas
+
+NC='\033[0m' # No Color
+BCYAN='\033[1;36m'
+BGREEN='\033[1;32m'
+BYELLOW='\033[1;33m'
+BRED='\033[1;31m'
+
+trap 'echo -e "❌ ${BYELLOW}Error occurred! ${BCYAN}Exiting...${NC}"' ERR
+
+sudo systemctl disable --now nginx.service 
     sudo rm -rf /usr/lib/nginx \
         /etc/nginx \
         /var/log/nginx \
         /var/cache/nginx \
-        /var/www/nginx &&
+        /var/www/nginx 
     sudo rm -f /usr/sbin/nginx \
         /var/run/nginx.pid \
         /var/run/nginx.lock \
-        /etc/systemd/system/nginx.service &&
+        /etc/systemd/system/nginx.service 
     sudo systemctl daemon-reload
