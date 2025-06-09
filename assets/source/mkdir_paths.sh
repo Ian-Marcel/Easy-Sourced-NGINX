eab86cfccde6df6826e66cab52fbad2ef59983cc
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-06-08 23:37:28 -0300

    23:37:28 - 08/06/2025

--- assets/source/mkdir_paths.sh
+++ assets/source/mkdir_paths.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo mkdir -p /etc/nginx /usr/lib/nginx/modules /var/log/nginx/ /run/nginx/ /var/cache/nginx/
-sudo chown -R nginx:nginx /etc/nginx /usr/lib/nginx/modules /var/log/nginx/ /run/nginx/ /var/cache/nginx/
+sudo mkdir -p /etc/nginx /usr/lib/nginx/modules /var/log/nginx/ /var/cache/nginx/
+sudo chown -R nginx:nginx /etc/nginx /usr/lib/nginx/modules /var/log/nginx/ /var/cache/nginx/
