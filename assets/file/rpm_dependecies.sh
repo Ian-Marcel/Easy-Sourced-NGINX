738dfa8bc95a6ebbaf03f1610830c94b139eca2b
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-06-08 23:16:05 -0300

    deb_deps & rpm_deps indented

--- assets/file/rpm_dependecies.sh
+++ assets/file/rpm_dependecies.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-dnf_tasks=(
-    "sudo yum upgrade --refresh --quiet --assumeyes"
-    "sudo yum --assumeyes --quiet group install development-tools"
-    "pcre"
-    "zlib"
-    "openssl"
-    "libxml2"
-    "libxslt"
+yum_tasks=(
+	"sudo yum upgrade --refresh --quiet --assumeyes"
+	"sudo yum --assumeyes --quiet group install development-tools"
+	"pcre"
+	"zlib"
+	"openssl"
+	"libxml2"
+	"libxslt"
 	"geoip"
-    "pcre-devel"
-    "zlib-devel"
-    "openssl-devel"
-    "libxml2-devel"
-    "libxslt-devel"
+	"pcre-devel"
+	"zlib-devel"
+	"openssl-devel"
+	"libxml2-devel"
+	"libxslt-devel"
 	"geoip-devel"
 )
