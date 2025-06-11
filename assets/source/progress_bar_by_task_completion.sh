70bc3019180a6ee16133713bda570f5dfb05cc87
Ian Marcel Penteado e Silva <ianmarcel@mailfence.com>
2025-06-11 18:39:17 -0300

    assets/source/progress_bar_by_task_completion.sh: variables outside(global) the funtion were turned into local(inside the funtion) variables

--- assets/source/progress_bar_by_task_completion.sh
+++ assets/source/progress_bar_by_task_completion.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
 
-bar_size=40
-bar_done_char="#"
-bar_todo_char="-"
-
 show_progress() {
-    local curr="$1"
-    local total="$2"
+	local bar_size=40
+	local bar_done_char="#"
+	local bar_todo_char="-"
+	local curr="$1"
+	local total="$2"
 
-    local percent=$(( 100 * curr / total ))
-    local done_slots=$(( curr * bar_size / total ))
-    local todo_slots=$(( bar_size - done_slots ))
+	local percent=$(( 100 * curr / total ))
+	local done_slots=$(( curr * bar_size / total ))
+	local todo_slots=$(( bar_size - done_slots ))
 
-    local done_str=""
-    for ((i = 0; i < done_slots; i++)); do
-        done_str+="$bar_done_char"
-    done
-    local todo_str=""
-    for ((i = 0; i < todo_slots; i++)); do
-        todo_str+="$bar_todo_char"
-    done
+	local done_str=""
+	for ((i = 0; i < done_slots; i++)); do
+		done_str+="$bar_done_char"
+	done
+	local todo_str=""
+	for ((i = 0; i < todo_slots; i++)); do
+		todo_str+="$bar_todo_char"
+	done
 
-    printf "\rProgress : [%s%s] %3d%%" \
-        "$done_str" "$todo_str" "$percent"
+	printf "\rProgress : [%s%s] %3d%%" \
+		"$done_str" "$todo_str" "$percent"
 
 	if [ "$curr" -eq "$total" ]; then
-        echo '' 
-    fi
+		echo '' 
+	fi
 }
 
