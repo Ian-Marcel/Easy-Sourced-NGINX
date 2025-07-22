c63d57c7d506ea063191e383ced971f91c3b9188
Ian <ianmarcel@mailfence.com>
2025-07-22 18:25:21 -0300

    18:25:21 - 22/07/2025

--- reinstall.sh
+++ reinstall.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-set -euo pipefail # Sair em caso de erro e falha em variáveis não definidas
+#### Error detector
+set -euo pipefail
+trap 'printf "\033[1;33mOps! \033[1;31m\b Something went wrong! \n\033[1;34mExiting...\033[0m\n' INT TERM ERR
+############
 
-NC='\033[0m' # No Color
-BCYAN='\033[1;36m'
-BGREEN='\033[1;32m'
-BYELLOW='\033[1;33m'
-BRED='\033[1;31m'
-CURL_ESX=2
-
-trap 'echo -e "❌ ${BYELLOW}Error occurred! ${BCYAN}Exiting...${NC}"' ERR
+NC='\033[0m'			# No Color
+BGREEN='\033[1;32m'		# Success Color
+BlBLUE='\033[1;34m'		# Info Color
+BCYAN='\033[1;36m'		# Link Color
+BYELLOW='\033[1;33m'	# Warning Color
+BRED='\033[1;31m'		# Error Color
 
 echo -e "${BCYAN}Backing up current nginx configuration folder, ${BYELLOW}it requires root access! ${NC}"
 sudo -v
